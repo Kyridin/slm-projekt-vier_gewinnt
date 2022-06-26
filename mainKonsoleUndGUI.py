b7f917f6a98b83fcb479d1bbc18141544c1f2d94
Kyridin <95933805+Kyridin@users.noreply.github.com>
2022-06-26 16:57:22 +0200

    Menüs hinzufügen und Spaltenauswahl
    Menüs und Spalten könne mit der Maus gesteuert werden.
    Hinzugefügt:
    - Startseite
    - Auswahl Schwierigkeit
    - Anschluss Menü/Gewinner Anzeige

--- mainKonsoleUndGUI.py
+++ mainKonsoleUndGUI.py
@@ -6,6 +6,12 @@ import pygame
 import sys
 import time
 
+# Spieleinstellungen die nur bei Beginn einmal eingestellt werden
+bot = False  # True: Bot aktiviert (1 Spieler); False: 2 Spieler
+bot_schwierigkeit = 1  # 0 = Easy ; 1 = Normal ; 2 = Hard
+spielen_auswahl = False
+schwierigkeit_auswahl = False
+
 while (True):
 
     # ----------------------------------------------------------------------
@@ -146,9 +152,9 @@ while (True):
 
     def bot_easy_move():
         """Bot, der zufällige moves wählt"""
-        for i in range(3):
-            print('.', end=' ')
-            time.sleep(.2)
+        # for i in range(3):
+        #    print('.', end=' ')
+        time.sleep(1)
         print('\n')
         spalte = random.randint(0, 6)
         print(spalte)
@@ -164,6 +170,7 @@ while (True):
         """
         # check ob Bot oder Spieler gewinnen kann und setze in die Spalte einen Stein
         move = suche_gewinner_move()[0]
+        time.sleep(1)
         print(move)
         if move != -1:
             return move
@@ -172,7 +179,12 @@ while (True):
 
 
     def bot_hard_move():
-        pass
+        move = suche_gewinner_move()[0]
+        time.sleep(1)
+        print(move)
+        if move != -1:
+            return move
+        return normal_distribution()
 
 
     def normal_distribution():
@@ -221,7 +233,7 @@ while (True):
     # initial_screen = pygame.display.set_mode([1270, 900])
 
     clock = pygame.time.Clock()
-    pygame.display.set_caption("Super krasses 4-gewinnt was dein Leben verÃ¤ndern wird")
+    pygame.display.set_caption("4-gewinnt Spiel")
 
 
     def drop_stone(dest_pos_index):
@@ -330,7 +342,7 @@ while (True):
     x_index = 4
 
     # this will be player one or two - the right side is the default value aka player which makes the first move
-    current_stone = roter_stein
+    # current_stone = roter_stein #nach unten verschoben! damit Stein abhänig vonder Konsole ist
 
     # static stuff - do that once instead of doing that every frame inside the loop
     background_surface.fill(WHITE)
@@ -384,25 +396,202 @@ while (True):
 
 
     # ----------------------------------------------------------------------
+    #                          Zugabe Buttons
+    # ----------------------------------------------------------------------
+
+    buttons_transparent = pygame.image.load("Buttons_Transparent.png")
+    hauptmenue_1 = pygame.image.load("Hauptmenue_1.png")
+    schwierigkeitsauswahl = pygame.image.load("Schwierigkeitsauswahl.png")
+    abschluss = pygame.image.load("Abschluss.png")
+
+    gelb_win = pygame.image.load("gelb_win.png")
+    rot_win = pygame.image.load("rot_win.png")
+    du_win = pygame.image.load("du_win.png")
+    com_win = pygame.image.load("com_win.png")
+    unent_win = pygame.image.load("unentschieden.png")
+    spalte_button = pygame.image.load("Spalte.png")
+
+
+    def draw_hauptmenue_surface():
+        screen.blit(background_surface, (0, 0))
+        # screen.blit(buttons_transparent, (366, 193))
+        screen.blit(hauptmenue_1, (366, 193))
+        pygame.display.update()
+
+
+    def draw_schwierigkeit_surface():
+        screen.blit(background_surface, (0, 0))
+        screen.blit(schwierigkeitsauswahl, (366, 193))
+        pygame.display.update()
+
+
+    def draw_abschluss_surface(anzeige_0):
+        screen.blit(abschluss, (366, 193))
+        if (anzeige_0 == 0):
+            screen.blit(gelb_win, (400, 267))
+            screen.blit(gelber_stein, (600, 100))
+        elif (anzeige_0 == 1):
+            screen.blit(rot_win, (400, 267))
+            screen.blit(roter_stein, (600, 100))
+        elif (anzeige_0 == 2):
+            screen.blit(com_win, (400, 267))
+            screen.blit(roter_stein, (600, 100))
+        elif (anzeige_0 == 3):
+            screen.blit(unent_win, (400, 267))
+            screen.blit(gelber_stein, (400, 100))
+            screen.blit(roter_stein, (800, 100))
+        elif (anzeige_0 == 4):
+            screen.blit(du_win, (400, 267))
+            screen.blit(gelber_stein, (600, 100))
+        pygame.display.update()
+
+
+    def button(screen, position, text, size, colors="white on blue"):
+        fg, bg = colors.split(" on ")
+        font = pygame.font.SysFont("Arial", size)
+        text_render = font.render(text, 1, fg)
+        x, y, w, h = text_render.get_rect()
+        x, y = position
+        pygame.draw.line(screen, (150, 150, 150), (x, y), (x + w, y), 5)
+        pygame.draw.line(screen, (150, 150, 150), (x, y - 2), (x, y + h), 5)
+        pygame.draw.line(screen, (50, 50, 50), (x, y + h), (x + w, y + h), 5)
+        pygame.draw.line(screen, (50, 50, 50), (x + w, y + h), [x + w, y], 5)
+        pygame.draw.rect(screen, bg, (x, y, w, h))
+        print(screen.blit(text_render, (x, y)))
+        return screen.blit(text_render, (x, y))
+
+
+    b1 = button(screen, (403, 375), "          ", 60, "red on yellow")
+    b2 = button(screen, (569, 375), "          ", 60, "blue on green")
+    b3 = button(screen, (735, 375), "          ", 60, "white on blue")
+
+
+    def button2(screen, position, text, size, colors="white on blue"):
+        x, y = position
+        w = 80
+        h = 800
+        return screen.blit(spalte_button, (x, y))
+
+
+    s0 = button2(screen, (326 - 10, 120), "", 20, "white on blue")
+    s1 = button2(screen, (421 - 10, 120), "", 20, "white on yellow")
+    s2 = button2(screen, (515 - 10, 120), "", 20, "white on green")
+    s3 = button2(screen, (609 - 10, 120), "", 20, "white on blue")
+    s4 = button2(screen, (703 - 10, 120), "", 20, "white on yellow")
+    s5 = button2(screen, (796 - 10, 120), "", 20, "white on green")
+    s6 = button2(screen, (891 - 10, 120), "", 20, "white on blue")
+
+
+    def start():
+        time.sleep(0.5)
+
+
+    # ----------------------------------------------------------------------
     #                                Game
     # ----------------------------------------------------------------------
+
+    # Einstellungen die immer wieder zurückgesetzt werden
     anfang = random.randint(0, 1)
     spieler = True if anfang == 0 else False  # True: Spieler 1; False: Spieler 2
-    bot = True  # True: Bot aktiviert (1 Spieler); False: 2 Spieler
-    bot_schwierigkeit = 1  # 0 = Easy ; 1 = Normal ; 2 = Hard
-    running = True  # 'e' als Input zum Beenden des Spiels
+    current_stone = gelber_stein if spieler else roter_stein
+    mouse_pressed = False
+    running = True
     gewinner = False
     unentschieden = False
-
-    # counter is sometimes useful for fixing things
+    abschluss_auswahl = False
+    anzeige_1 = -1
     counter = 0
 
     while (running):
+
+        while (not spielen_auswahl):
+            for event in pygame.event.get():
+                if (event.type == pygame.QUIT):
+                    pygame.quit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        pygame.quit()
+                    key_to_start = event.key == pygame.K_s
+                    if key_to_start:
+                        start()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    # check when you click if the coordinates of the pointer are in the rectangle of the buttons
+                    if b1.collidepoint(pygame.mouse.get_pos()):
+                        start()
+                        bot = False
+                        spielen_auswahl = True
+                        schwierigkeit_auswahl = True
+                        break
+                    elif b2.collidepoint(pygame.mouse.get_pos()):
+                        start()
+                        bot = True
+                        bot_schwierigkeit = 1
+                        spielen_auswahl = True
+                        break
+                    elif b3.collidepoint(pygame.mouse.get_pos()):
+                        pygame.quit()
+            draw_hauptmenue_surface()
+
+        while (not schwierigkeit_auswahl):
+            for event in pygame.event.get():
+                if (event.type == pygame.QUIT):
+                    pygame.quit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        pygame.quit()
+                    key_to_start = event.key == pygame.K_s
+                    if key_to_start:
+                        start()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    # check when you click if the coordinates of the pointer are in the rectangle of the buttons
+                    if b1.collidepoint(pygame.mouse.get_pos()):
+                        start()
+                        bot_schwierigkeit = 0
+                        schwierigkeit_auswahl = True
+                        break
+                    elif b2.collidepoint(pygame.mouse.get_pos()):
+                        start()
+                        bot_schwierigkeit = 1
+                        schwierigkeit_auswahl = True
+                        start()
+                        break
+                    elif b3.collidepoint(pygame.mouse.get_pos()):
+                        bot_schwierigkeit = 2
+                        schwierigkeit_auswahl = True
+                        start()
+            draw_schwierigkeit_surface()
 
         # standard exit/break out the loop
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            # check when you click if the coordinates of the pointer are in the rectangle of the buttons
+            if s0.collidepoint(pygame.mouse.get_pos()):
+                mouse_pressed = True
+                spalte = 0
+            elif s1.collidepoint(pygame.mouse.get_pos()):
+                mouse_pressed = True
+                spalte = 1
+            elif s2.collidepoint(pygame.mouse.get_pos()):
+                mouse_pressed = True
+                spalte = 2
+            elif s3.collidepoint(pygame.mouse.get_pos()):
+                mouse_pressed = True
+                spalte = 3
+            elif s4.collidepoint(pygame.mouse.get_pos()):
+                mouse_pressed = True
+                spalte = 4
+            elif s5.collidepoint(pygame.mouse.get_pos()):
+                mouse_pressed = True
+                spalte = 5
+            elif s6.collidepoint(pygame.mouse.get_pos()):
+                mouse_pressed = True
+                spalte = 6
 
         pressed_button_vert = pygame.key.get_pressed()
         if pressed_button_vert[pygame.K_RIGHT]:
@@ -422,7 +611,7 @@ while (True):
                 x -= x_spacing_for_76
                 time.sleep(0.07)
 
-        if pressed_button_vert[pygame.K_RETURN] or (bot and not spieler):
+        if pressed_button_vert[pygame.K_RETURN] or (bot and not spieler) or mouse_pressed:
 
             # --------------------------------------------------
             if bot and not spieler:
@@ -432,13 +621,17 @@ while (True):
                     spalte = bot_normal_move()
                 else:
                     spalte = bot_hard_move()
+            elif mouse_pressed:
+                pass
             else:
                 spalte = x_index - 1
+
+            mouse_pressed = False
 
             if spalte_gueltig(spalte):
 
                 print("x: ", x, ", spalte: ", spalte)
-                x = convert_spalte_x_bot[spalte]  # Achtung hier können noch fehler auftreten! Keine Ahnung wie?
+                x = convert_spalte_x_bot[spalte]
 
                 zeile = finde_tiefste_zeile(spalte)
                 print(spalte, zeile)
@@ -456,7 +649,6 @@ while (True):
                     unentschieden = True
                 # --------------------------------------------------
 
-                # Achtung hier können noch fehler auftreten! Keine Ahnung wie?
                 drop_stone(convert_zeile_dropStone[zeile])  # Number of Falling Rows
                 x = convert_spalte_x_bot[x_index - 1]
                 print(x, "|", y)
@@ -476,15 +668,47 @@ while (True):
         counter += 1
         # print(counter)
         # -------------------------------------------
-    pygame.time.wait(5000)
+    pygame.time.wait(2500)
 
-    counter += 1
+    if (unentschieden):
+        anzeige_1 = 3
+    elif (bot and spieler):
+        anzeige_1 = 2
+    elif (bot):
+        anzeige_1 = 4
+    elif (not spieler):
+        anzeige_1 = 0
+    elif (spieler):
+        anzeige_1 = 1
+
+    while (not abschluss_auswahl):
+        for event in pygame.event.get():
+            if (event.type == pygame.QUIT):
+                pygame.quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                key_to_start = event.key == pygame.K_s
+                if key_to_start:
+                    start()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                # check when you click if the coordinates of the pointer are in the rectangle of the buttons
+                if b1.collidepoint(pygame.mouse.get_pos()):
+                    spielen_auswahl = True
+                    schwierigkeit_auswahl = True
+                    abschluss_auswahl = True
+
+                    break
+                elif b2.collidepoint(pygame.mouse.get_pos()):
+                    spielen_auswahl = False
+                    schwierigkeit_auswahl = False
+                    abschluss_auswahl = True
+
+                    break
+                elif b3.collidepoint(pygame.mouse.get_pos()):
+                    pygame.quit()
+
+        draw_abschluss_surface(anzeige_1)
+
+    # counter += 1
     # print(counter)
-    '''
-    spalte = input("Beenden: ")
-    if spalte == 'j':
-        pygame.quit()
-        break
-    else:
-        pass
-    '''
